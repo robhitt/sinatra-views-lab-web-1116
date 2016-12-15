2abe7c9df89ca86f7dd94dbbee24901d77b1dd59
Rob Hitt <robhitt@gmail.com>
2016-12-15 13:50:52 -0500

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
+
+
 class App < Sinatra::Base
+
+
+	get '/date' do
+		erb :date
+	end
+
+
+	get '/goodbye' do
+		erb :goodbye
+	end
+
+
+	get '/hello' do
+		erb :hello
+	end
+
 
 	get '/' do
 		erb :index
